d81254888e3981a0daa721304644ef0d2b836d10
jmsaulnier <jmsaulnier@users.noreply.github.com>
2020-11-28 06:57:13 +0000

    deploy: f0bf69810cabc34cab3882e9c6c639169c88fbd7

--- _next/static/Lj6nW67ZIypwSDr2G7pXJ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-b62e47e8a131a83ccc33.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-3ca3d9788ec4c80fcb7b.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
